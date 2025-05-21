b03c4b1ee109712ea1854849c0d71b3c98441b6c
Raphael <raroth@student.ethz.ch>
2025-05-21 10:55:22 +0200

    (feat) introduced multicast / reduction test program

---
+++ sw/snitch/tests/reduction.c
@@ -0,0 +1,57 @@
+// Copyright 2023 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Raphael Roth <raroth@student.ethz.ch>
+//
+// This testbench aims to test the reduction feature in the narrow interconnect.
+// The core run an integer add reduction between cluster 0 and cluster 1 with the destination cluster 3
+// It works only on the mini_picobello configuration as it is address dependent!
+
+// Src Adr Cluster 0: 48'h000020000000
+// Src Adr Cluster 1: 48'h000020040000
+// Dst Adr Cluster 3: 48'h0000200c0000
+
+#include <stdint.h>
+#include "picobello_addrmap.h"
+#include "snrt.h"
+
+int main (void){
+    snrt_int_clr_mcip();
+
+    // Get core id
+    uint32_t core_id = snrt_cluster_core_idx();
+	uint32_t cluster_id = snrt_cluster_idx();
+
+	// Generate the mask (take the difference in the start address should be sufficient)
+	uint32_t mask = 0x00040000;
+
+	// Determint an end address in the adr range of the 3rd cluster
+	uintptr_t addr = 0x0000200c0100;
+
+	// We only want to work with core 0's
+    if(core_id == 0){
+		// TODO raroth: run the reducion par here
+		if((cluster_id == 0) || (cluster_id == 1)){
+			snrt_enable_reduction(mask, 6);				// Enable Integer Add reduction
+			*((uint32_t *) addr) = cluster_id + 2;
+			snrt_disable_reduction();
+		}
+	}
+
+	// Sync up all cores across cluster
+	snrt_global_barrier();
+	//snrt_cluster_hw_barrier(); //TODO: Is this only inside the cluser or with all cores?
+	//snrt_inter_cluster_barrier(); // TODO Verify with Lorenzo if correct
+
+	// Sanity check - Core 0 from Cluster 3 should read 5 from the addr!
+	if((core_id == 0) && (cluster_id == 3)){
+		if(*((uint32_t *) addr) == 5){
+			return 0;
+		} else {
+			return 1;
+		}
+	} else {
+		return 0;
+	}
+}

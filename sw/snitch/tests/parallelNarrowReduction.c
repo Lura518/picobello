cb226cfde16054349ab666f878c701e1f757f50d
Raphael <raroth@student.ethz.ch>
2025-05-21 17:38:46 +0200

    (feat) two tests which test the offload and the parallel reduction. They should be called with the simple_offload.spm.elf for the cheshire subsystem.

--- sw/snitch/tests/parallelNarrowReduction.c
+++ sw/snitch/tests/parallelNarrowReduction.c
@@ -4,8 +4,8 @@
 //
 // Raphael Roth <raroth@student.ethz.ch>
 //
-// This testbench aims to test the reduction feature in the narrow interconnect.
-// The core run an integer add reduction between cluster 0 and cluster 1 with the destination cluster 3
+// This testbench aims to test the parallel reduction feature in the narrow interconnect.
+// The core run an lsb and reduction between cluster 0 and cluster 1 with the destination cluster 3
 // It works only on the mini_picobello configuration as it is address dependent!
 
 // Src Adr Cluster 0: 48'h000020000000
@@ -29,24 +29,30 @@ int main (void){
 	// Determint an end address in the adr range of the 3rd cluster
 	uintptr_t addr = 0x0000200c0100;
 
+	// Set a default value for the target address
+	if((core_id == 0) && (cluster_id == 3)){
+		*((uint32_t *) addr) = 3;
+	}
+
+	// Sync up all cores across cluster
+	snrt_global_barrier();
+
 	// We only want to work with core 0's
     if(core_id == 0){
 		// TODO raroth: run the reducion par here
 		if((cluster_id == 0) || (cluster_id == 1)){
-			snrt_enable_reduction(mask, 6);				// Enable Integer Add reduction
-			*((uint32_t *) addr) = cluster_id + 2;
+			snrt_enable_reduction(mask, 12);				// Enable Parallel Reduction
+			*((uint32_t *) addr) = 1;
 			snrt_disable_reduction();
 		}
 	}
 
 	// Sync up all cores across cluster
 	snrt_global_barrier();
-	//snrt_cluster_hw_barrier(); //TODO: Is this only inside the cluser or with all cores?
-	//snrt_inter_cluster_barrier(); // TODO Verify with Lorenzo if correct
 
 	// Sanity check - Core 0 from Cluster 3 should read 5 from the addr!
 	if((core_id == 0) && (cluster_id == 3)){
-		if(*((uint32_t *) addr) == 5){
+		if(*((uint32_t *) addr) == 1){
 			return 0;
 		} else {
 			return 1;

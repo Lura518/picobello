bca4abd41343394ccb5a950367b2a925d7cdb414
Raphael <raroth@student.ethz.ch>
2025-07-08 17:22:29 +0200

    (sw) new benchmarks
    * 1 stage > Generic & Stalling controller implementation
    * 2 stage > Simple controller implementation

---
+++ sw/snitch/tests/bench_hw_reduction_1stage.c
@@ -0,0 +1,129 @@
+// Copyright 2025 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Raphael Roth <raroth@student.ethz.ch>
+//
+// This code can be used to benchmark the hardware reduction feature of the picobello 
+// system. It can reduce double data directly in fligth with the internal DMA.
+// Is is possible to set the target cluster freely.
+// The reduction is done in one stage for all clusters.
+
+#include <stdint.h>
+#include "picobello_addrmap.h"
+#include "snrt.h"
+
+// Benchmark Parameter:
+#ifndef NUMBER_OF_CLUSTERS
+#define NUMBER_OF_CLUSTERS              8   // Needs to be either 2 / 4 / 8 / 16
+#endif
+
+#ifndef TARGET_CLUSTER
+#define TARGET_CLUSTER                  6
+#endif
+
+#ifndef DATA_BYTE
+#define DATA_BYTE                       512
+#endif
+
+// Translate from byte into doubles
+#ifndef DATA_LENGTH
+#define DATA_LENGTH                     (DATA_BYTE/8)
+#endif
+
+#define DATA_EVAL_LENGTH    (DATA_LENGTH)
+
+// Define the reduction mask depending on the number of involved clusters
+#define REDUCTION_MASK      ((NUMBER_OF_CLUSTERS - 1) << 18)
+
+/**
+ * @brief Return if the cluster is involved in the reduction or not.
+ * @param cluster_nr cluster id
+ */
+static inline int cluster_participates_in_reduction(int cluster_nr) {
+    return (cluster_nr < NUMBER_OF_CLUSTERS);
+}
+
+// Main code
+int main() {
+    snrt_interrupt_enable(IRQ_M_CLUSTER);
+
+    // Sanity check:
+    if(((NUMBER_OF_CLUSTERS % 4) != 0) && (NUMBER_OF_CLUSTERS != 12)){
+        // Number of cluster should be a power of 2 (for mask generation)
+        return 1;
+    }
+
+    // Cluster ID
+    uint32_t cluster_id = snrt_cluster_idx();
+
+    // Set the mask for the multicast
+    uint32_t mask = REDUCTION_MASK;
+
+    // Generate unique data for the reduction
+    double fill_value = 42.0;
+    double init_data = 15.0 + (double) cluster_id;
+
+    // Allocate destination buffer
+    double *buffer_dst = (double*) snrt_l1_next_v2();
+    double *buffer_src = buffer_dst + DATA_LENGTH;
+
+    // Determint the target address
+    double *buffer_target = (double*) snrt_remote_l1_ptr(buffer_dst, cluster_id, TARGET_CLUSTER);
+    volatile double *buffer_last_entry = (volatile double *) (buffer_dst + DATA_LENGTH - 1);
+
+    // Fill the source buffer with the init data
+    if (snrt_is_dm_core()) {
+        for (uint32_t i = 0; i < DATA_LENGTH; i++) {
+            buffer_src[i] = init_data + (double) i;
+            buffer_dst[i] = fill_value;
+        }
+    }
+
+    // Wait until the cluster are finished
+    snrt_global_barrier();
+
+    // Do the transmission 3 times to preheat the cache
+    for(volatile int i = 0; i < 3; i++){
+        // Reset the last entry from prior loop (if @ end of loop > check at end fails)
+        if((cluster_id == TARGET_CLUSTER) && snrt_is_dm_core()){
+            *buffer_last_entry = fill_value;
+        }
+
+        // Perf. analysis
+        snrt_mcycle();
+        
+        // Init the DMA multicast
+        if (snrt_is_dm_core() && cluster_participates_in_reduction(cluster_id)) {
+            snrt_dma_start_1d_collective(buffer_target, buffer_src, DATA_LENGTH * sizeof(double), (void *) mask, SNRT_COLL_WIDE_FPADD);
+            // Only waits until last w is transmitted but not until b response arrives!
+            snrt_dma_wait_all();
+        }
+
+        // Target cluster polls the last array entry to know when we wrote the data
+        if((cluster_id == TARGET_CLUSTER) && snrt_is_dm_core()){
+            while(*buffer_last_entry == fill_value);
+        }
+
+        // Perf. analysis
+        snrt_mcycle();
+
+        // Sync all cores
+        snrt_global_barrier();
+    }
+
+    // Evaluate the reduction result
+    if (snrt_is_dm_core() && (cluster_id == TARGET_CLUSTER)) {
+        uint32_t n_errs = DATA_EVAL_LENGTH;
+        double base_value = (NUMBER_OF_CLUSTERS*15.0) + (double) (((NUMBER_OF_CLUSTERS-1) * ((NUMBER_OF_CLUSTERS-1) + 1)) >> 1);
+        for (uint32_t i = 0; i < DATA_EVAL_LENGTH; i++) {
+            if (buffer_dst[i] == base_value){
+                n_errs--;
+            }
+            base_value = base_value + (double) NUMBER_OF_CLUSTERS;
+        }
+        return n_errs;
+    } else {
+        return 0;
+    }
+}
--- sw/snitch/tests/bench_sw_reduction.c
+++ sw/snitch/tests/bench_sw_reduction.c
@@ -1,6 +1,19 @@
+// Copyright 2025 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Raphael Roth <raroth@student.ethz.ch>
+//
+// This code can be used to benchmark the reduction feature of the picobello system.
+// It supports different kinds of reduction pattern to compare them against each other.
 
+#include <stdint.h>
+#include "picobello_addrmap.h"
+#include "snrt.h"
 
-
+#ifndef SW_METHODE
+#define SW_METHODE   STAR
+#endif
 
 // Use the SSR to reduce the data!
 
